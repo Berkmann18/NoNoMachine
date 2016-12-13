6857671484d649c73fee12e49edb1c0c186bd017
Berkmann18 <maxieberkmann@gmail.com>
2016-12-13 21:28:02 +0000

    Added

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -5,3 +5,4 @@ sed -i 's/abcd123/zcvd321/g' SSH_config.txt
 sed -i 's/abcd123/zcvd321/g' psqlConnect.sh
 sed -i 's/abcd123/zcvd321/g' sshDB.sh
 sed -i 's/abcd123/zcvd321/g' cim.sh
+cp SSH_config.txt ~/.ssh/config
